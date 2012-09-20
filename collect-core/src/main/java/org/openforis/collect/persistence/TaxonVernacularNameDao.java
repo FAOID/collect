27b20f11bb07b0a1e9f1c069659b74b5479e3ebf
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2012-09-20 13:14:39 +0700

    Fix / choose a better vernacular name sorting technique
    Signed-off-by: Eko Suprapto Wibowo <swdev.bali@gmail.com>

--- collect-core/src/main/java/org/openforis/collect/persistence/TaxonVernacularNameDao.java
+++ collect-core/src/main/java/org/openforis/collect/persistence/TaxonVernacularNameDao.java
@@ -42,7 +42,7 @@ public class TaxonVernacularNameDao extends MappingJooqDaoSupport<TaxonVernacula
 	public List<TaxonVernacularName> findByVernacularName(int taxonomyId, String searchString, HashMap<String, String> hashQualifier, int maxResults) {
 		JooqFactory jf = getMappingJooqFactory();
 		//find containing
-		searchString = "%" + searchString.toUpperCase() + "%";
+		searchString = searchString.toUpperCase() + "%";
 		
 		SelectConditionStep selectConditionStep = jf.select(OFC_TAXON_VERNACULAR_NAME.getFields())
 			.from(OFC_TAXON_VERNACULAR_NAME)
